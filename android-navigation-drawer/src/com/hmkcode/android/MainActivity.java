68896f1413780b73fa6e83f0c3b6551fd6517702
Michael Strelnikov <michaelst@gmail.com>
2014-06-25 15:29:37 +1000

    made it working

--- android-navigation-drawer/src/com/hmkcode/android/MainActivity.java
+++ android-navigation-drawer/src/com/hmkcode/android/MainActivity.java
@@ -1,14 +1,14 @@
 package com.hmkcode.android;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.res.Configuration;
+import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -26,6 +26,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_ACTION_BAR);
 		setContentView(R.layout.activity_main);
 		
 		// get list items from strings.xml
